e8ee8dde06192ae692e32262d522b23afffb2b6b
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-09-18 16:08:18 -0400

    a

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 let dogBar = document.querySelector("div#dog-bar")
 let dogInfo = document.querySelector("div#dog-info")
 
+
 fetch("http://localhost:3000/pups")
     .then(res => res.json())
     .then(pupsObj => {
@@ -11,26 +12,45 @@ fetch("http://localhost:3000/pups")
         })
     })
 
+//   let pupShowpage = () => {    
+//     let pupImg = document.createElement('img')
+//     let pupNameH2 = document.createElement('h2')
+//     let pupButton = document.createElement('button')
+   
+//   }
+    
+  
 let turnObjToHTML = (pup) => {
     let pupSpan = document.createElement('span')
     pupSpan.innerText = pup.name
+    pupSpan.id = "pupNameSpanBar"
     dogBar.append(pupSpan)
     pupSpan.style.cursor = "pointer"
 
+      
     let pupImg = document.createElement('img')
     let pupNameH2 = document.createElement('h2')
     let pupButton = document.createElement('button')
+    // dogInfo.append(pupImg, pupNameH2, pupButton)   
 
-  
     pupSpan.addEventListener("click", (evt)=>{
-        pupImg.src = pup.image
-        pupNameH2.innerText = pup.name
-        pupButton.innerText = pup.isGoodDog ? "Good Dog" : "Bad Dog"
-        dogInfo.append(pupImg)
-        dogInfo.append(pupNameH2)
-        dogInfo.append(pupButton)
+        fetch(`http://localhost:3000/pups/${pup.id}`)
+        .then(res => res.json())
+        .then(pupShowObj => {
+            pupImg.src = pupShowObj.image
+            pupNameH2.innerText = pupShowObj.name
+            pupButton.innerText = pupShowObj.isGoodDog ? "Good Dog" : "Bad Dog"
+            dogInfo.append(pupImg, pupNameH2, pupButton)  
+        })
     })
 
+    // pupSpan.addEventListener("click", (evt)=>{
+    //     pupImg.src = pup.image
+    //     pupNameH2.innerText = pup.name
+    //     pupButton.innerText = pup.isGoodDog ? "Good Dog" : "Bad Dog"
+    //     dogInfo.append(pupImg, pupNameH2, pupButton)  
+    // })
+    
     pupButton.addEventListener("click", (evt)=>{
         let newGoodOrBad = !pup.isGoodDog 
         //update the backend
@@ -54,7 +74,21 @@ let turnObjToHTML = (pup) => {
     })
 
 
-
 }    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
